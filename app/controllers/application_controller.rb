dd4668e1fcc3bd03ac2dd4d41cdc7ca67fa99acb
Madison <madivallarino@gmail.com>
2021-10-03 16:25:03 -0400

    commit tree d9bade6c3efd33aed894bbb1e53ea7d42f0f815e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    "Hello World"
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do 
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: {baked_goods: {include: :bakery}})
+  end
+
+get '/baked_goods/by_price' do 
+  bakedgoods = BakedGood.all.order(:price)
+  bakedgoods.to_json
+end
+
+get '/baked_goods/most_expensive' do
+  #and then return one which I cant figure out so whatever
+  bakedgood = BakedGood.all.order(:price)
+
+  bakedgood.to_json
+end
+
 
 end
